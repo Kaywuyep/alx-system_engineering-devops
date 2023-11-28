2f9a609fbe24aa88f5aebaa06db62f973c84b482
Kaywuyep <wuyepkate@gmail.com>
2023-11-28 05:19:26 -0800

    task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,3 +1,7 @@
 #!/usr/bin/env ruby
-# a simple TextMe exercise
+# a simple TextMe exercise using the non-greedy way
+# \[(.*?)\] -  non-greedy match for any char (except newline) within d brackets
+# \s - matches whitespace characters
+# join(',')- This joins the captured groups for each
+# match using a comma as a separator.
 puts ARGV[0].scan(/\[from:(.*?)\]\s\[to:(.*?)\]\s\[flags:(.*?)\]/).join(',')
