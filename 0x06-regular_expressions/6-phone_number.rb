50c33606aa4bbbfa9661ea7a1d23f0ee49b0e4b2
Kaywuyep <wuyepkate@gmail.com>
2023-11-28 04:53:50 -0800

    task

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-#  regular expression must match a 10 digit phone number
+#  regular expression must match a 10 digit phone number '/^[0-9]{10]$/'
 puts ARGV[0].scan(/^\d{10}$/).join
