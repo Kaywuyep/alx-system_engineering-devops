e6ea2befbd3ef543d33a0dfbbefe6ebf97e2f01a
Kaywuyep <wuyepkate@gmail.com>
2024-01-19 06:36:03 -0800

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
 # using puppet to create a file
 
-file {'/tmp/school':
-ensure  => 'file'
-mode    => '0744',
-owner   => 'www-data',
-group   => 'www-data',
-content => 'I love Puppet',
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
