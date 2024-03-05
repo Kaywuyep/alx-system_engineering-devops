070137c7dcac6dcabce7160fb4e5b4eadcb6822b
Kaywuyep <wuyepkate@gmail.com>
2024-03-05 12:37:17 -0800

    project tasks

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" a module that counts sorted keywords"""
+import requests
+
+
+def count_words(subreddit, word_list):
+    """
+    returns the number of sorted count words recuesive
+    """
+    return _count_words_recursive(subreddit, word_list)
+
+def _count_words_recursive(subreddit, word_list, after=None, counts=None):
+    """
+    sorted count of given keywords
+    """
+    if counts is None:
+        counts = {}
+
+    if after is None:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
+    else:
+        url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100&after={after}"
+    
+    headers = {'User-Agent': 'Custom User Agent'}
+
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+
+        posts = data['data']['children']
+        for post in posts:
+            title = post['data']['title'].lower()
+            for word in word_list:
+                if word.lower() in title.split():
+                    if word.lower() in counts:
+                        counts[word.lower()] += 1
+                    else:
+                        counts[word.lower()] = 1
+
+        after = data['data']['after']
+        if after:
+            return _count_words_recursive(subreddit, word_list, after, counts)
+        else:
+            sorted_counts = sorted(counts.items(), key=lambda x: (-x[1], x[0]))
+            for word, count in sorted_counts:
+                print(f"{word}: {count}")
+    except requests.exceptions.RequestException as e:
+        pass
